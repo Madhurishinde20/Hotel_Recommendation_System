589b139efacecccddad869537c0f5f39abdf5bb9
Madhuri Shinde <mshinde2442003@gmail.com>
2025-06-11 12:36:44 +0530

    some changes done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let app=require("./src/app.js");
 
-let port=2000;
-app.listen(port,(res,req)=>{
-   console.log("Server started "  + port);
+require('dotenv').config();
+
+app.listen(process.env.PORT,()=>{
+   console.log("Server started ");
 });
--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 let express=require("express");
-
 let bodyparser=require("body-parser");
-
 let cookieParser=require("cookie-parser");
+let router=require("../src/routes/userRoutes.js");
 
 let conn=require("./config/db.js");
-
-let session=require("express-session");
-
-let router = require("./routes/userRouter");
-
 let app=express();
 
 app.set('view engine','ejs');
@@ -17,12 +11,6 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended:true}));
 app.use(bodyparser.json());
 app.use(cookieParser());
-
-app.use(session({
-    secret: "1$23$45",
-    resave: false,
-    saveUninitialized: true
-}));
 app.use("/", router);
 
 module.exports=app;
